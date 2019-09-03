f6d9b01243be0c2f6beb494df25e727d1b348789
saidileeppr <43446416+saidileeppr@users.noreply.github.com>
2019-09-03 16:20:43 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import re
+import os
+def downimg(i,name,ch,ns,pg):
+    path=os.getcwd()
+    path1=path+'/'+ns+'/'
+    os.system('wget -O '+path1+str(ch)+'-'+str(pg)+'.jpg'+'  '+i)
+    f=open(path1+name,'a+')
+    f.write('\t\t<div>\n\t\t\t<center>\n')
+    f.write('\t\t\t\t<img  src='+str(ch)+'-'+str(pg)+'.jpg'+' >\n')
+    f.write('\t\t\t</center>\n\t\t</div>\n')
+def getimg(ch,ln,ns):
+    il=[]
+    pg=1
+    path=os.getcwd()
+    path1=path+'/'+ns+'/'
+    link=ln
+    wd=webdriver.Chrome(executable_path='/bin/chromedriver')
+    wd.get(link)
+    time.sleep(10)
+    page=wd.page_source
+    wd.close()
+    soup=BeautifulSoup(page,'lxml')
+    for i in soup.findAll('img'):
+        s=i.get('src')
+        l=re.findall('https://2.bp.blogspot.com*',str(s))
+        if(l):
+            il.append(str(s))
+    if(il):
+        pass
+    else:
+        print('Error Occured for chapter '+str(ch))
+        return 0
+    h1='''<!DOCTYPE html>
+<html>
+	<body bgcolor='#262626'>
+'''
+    name=str(ch)+'.html'
+    os.system('mkdir '+ns)
+    f=open(path1+name,'w+')
+    f.write(h1)
+    f.close()
+    e1='''	</body>	
+</html>'''
+    for i in il:
+        downimg(i,name,ch,ns,pg)
+        pg=pg+1
+    f=open(path1+name,'a+')
+    f.write(e1)
+    f.close()
+ml=str(input('Enter link:'))
+mi=int(input('Enter starting:'))
+ma=int(input('Enter ending:'))
+for ch in range(mi,ma+1):
+    if(ch<10):
+        ln=ml+'/Ch-00'+str(ch)
+    elif(ch>9 and ch<100):
+        ln=ml+'/Ch-0'+str(ch)
+    else:
+        ln=ml+'/Ch-'+str(ch)
+    f=ln.split('/')
+    ns=f[4]
+    getimg(ch,ln,ns)
